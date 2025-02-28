cdc7e38a33bafbed1f3bae251f8228e402f1902b
kennyluke1023 <78526876+kennyluke1023@users.noreply.github.com>
2025-02-28 16:37:09 +0800

    Create activate_py.sh

---
+++ bash/activate_py.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Check if an argument is provided
+if [ -z "$1" ]; then
+  echo "Usage: source activate.sh <venv_name>"
+  exit 1
+fi
+venv_name="$1"
+# Search for the virtual environment directory
+venv_path=$(find . -name "$venv_name" -type d -print -quit)
+# Check if the virtual environment was found
+if [ -z "$venv_path" ]; then
+  echo "Virtual environment '$venv_name' not found."
+  exit 1
+fi
+# Construct the path to the activate script
+activate_script="$venv_path/bin/activate"
+echo "$activate_script"
+source "$activate_script"
